08ffbbab5e3823cc2210f40cdef2416d1e5028f8
rlazmar05 <rublazmar@alum.us.es>
2025-06-24 14:03:24 +0200

    Update 4

---
+++ bitbucket/src/main/java/aiss/bitbucket/model/issues/IssueComments.java
@@ -0,0 +1,221 @@
+package aiss.bitbucket.model.issues;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import aiss.bitbucket.model.User;
+import aiss.bitbucket.model.issues.Inline;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import javax.swing.text.AbstractDocument;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "type",
+        "id",
+        "created_on",
+        "updated_on",
+        "content",
+        "user",
+        "deleted",
+        "inline",
+        "links",
+        "issue"
+})
+public class IssueComments {
+
+    @JsonProperty("type")
+    private String type;
+    @JsonProperty("id")
+    private Integer id;
+    @JsonProperty("created_on")
+    private String createdOn;
+    @JsonProperty("updated_on")
+    private String updatedOn;
+    @JsonProperty("content")
+    private Content content;
+    @JsonProperty("user")
+    private User user;
+    @JsonProperty("deleted")
+    private Boolean deleted;
+    @JsonProperty("inline")
+    private Inline inline;
+    @JsonProperty("links")
+    private Links links;
+    @JsonProperty("issue")
+    private Issue issue;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new LinkedHashMap<String, Object>();
+
+    @JsonProperty("type")
+    public String getType() {
+        return type;
+    }
+
+    @JsonProperty("type")
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @JsonProperty("id")
+    public Integer getId() {
+        return id;
+    }
+
+    @JsonProperty("id")
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @JsonProperty("created_on")
+    public String getCreatedOn() {
+        return createdOn;
+    }
+
+    @JsonProperty("created_on")
+    public void setCreatedOn(String createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    @JsonProperty("updated_on")
+    public String getUpdatedOn() {
+        return updatedOn;
+    }
+
+    @JsonProperty("updated_on")
+    public void setUpdatedOn(String updatedOn) {
+        this.updatedOn = updatedOn;
+    }
+
+    @JsonProperty("content")
+    public Content getContent() {
+        return content;
+    }
+
+    @JsonProperty("content")
+    public void setContent(Content content) {
+        this.content = content;
+    }
+
+    @JsonProperty("user")
+    public User getUser() {
+        return user;
+    }
+
+    @JsonProperty("user")
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @JsonProperty("deleted")
+    public Boolean getDeleted() {
+        return deleted;
+    }
+
+    @JsonProperty("deleted")
+    public void setDeleted(Boolean deleted) {
+        this.deleted = deleted;
+    }
+
+    @JsonProperty("inline")
+    public Inline getInline() {
+        return inline;
+    }
+
+    @JsonProperty("inline")
+    public void setInline(Inline inline) {
+        this.inline = inline;
+    }
+
+    @JsonProperty("links")
+    public Links getLinks() {
+        return links;
+    }
+
+    @JsonProperty("links")
+    public void setLinks(Links links) {
+        this.links = links;
+    }
+
+    @JsonProperty("issue")
+    public Issue getIssue() {
+        return issue;
+    }
+
+    @JsonProperty("issue")
+    public void setIssue(Issue issue) {
+        this.issue = issue;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(IssueComments.class.getName()).append('@').append(Integer.toHexString(System.identityHashCode(this))).append('[');
+        sb.append("type");
+        sb.append('=');
+        sb.append(((this.type == null)?"<null>":this.type));
+        sb.append(',');
+        sb.append("id");
+        sb.append('=');
+        sb.append(((this.id == null)?"<null>":this.id));
+        sb.append(',');
+        sb.append("createdOn");
+        sb.append('=');
+        sb.append(((this.createdOn == null)?"<null>":this.createdOn));
+        sb.append(',');
+        sb.append("updatedOn");
+        sb.append('=');
+        sb.append(((this.updatedOn == null)?"<null>":this.updatedOn));
+        sb.append(',');
+        sb.append("content");
+        sb.append('=');
+        sb.append(((this.content == null)?"<null>":this.content));
+        sb.append(',');
+        sb.append("user");
+        sb.append('=');
+        sb.append(((this.user == null)?"<null>":this.user));
+        sb.append(',');
+        sb.append("deleted");
+        sb.append('=');
+        sb.append(((this.deleted == null)?"<null>":this.deleted));
+        sb.append(',');
+        sb.append("inline");
+        sb.append('=');
+        sb.append(((this.inline == null)?"<null>":this.inline));
+        sb.append(',');
+        sb.append("links");
+        sb.append('=');
+        sb.append(((this.links == null)?"<null>":this.links));
+        sb.append(',');
+        sb.append("issue");
+        sb.append('=');
+        sb.append(((this.issue == null)?"<null>":this.issue));
+        sb.append(',');
+        sb.append("additionalProperties");
+        sb.append('=');
+        sb.append(((this.additionalProperties == null)?"<null>":this.additionalProperties));
+        sb.append(',');
+        if (sb.charAt((sb.length()- 1)) == ',') {
+            sb.setCharAt((sb.length()- 1), ']');
+        } else {
+            sb.append(']');
+        }
+        return sb.toString();
+    }
+
+}
