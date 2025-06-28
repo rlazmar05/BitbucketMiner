157ac5518ab6636894379684b0ea3a2a0d858d71
Carlos Aguilar Raya <carlosarsafa@gmail.com>
2025-06-28 18:15:15 +0200

    update

--- bitbucket/src/main/java/aiss/bitbucket/controllers/BitbucketMinerController.java
+++ bitbucket/src/main/java/aiss/bitbucket/controllers/BitbucketMinerController.java
@@ -2,8 +2,7 @@ package aiss.bitbucket.controllers;
 
 import aiss.bitbucket.model.commits.Commit;
 import aiss.bitbucket.model.issues.Issue;
-import aiss.bitbucket.service.BitbucketService;
-import aiss.bitbucket.service.GitHubService;
+import aiss.bitbucket.service.*;
 import aiss.bitbucket.service.GitHubService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -20,7 +19,13 @@ public class BitbucketMinerController {
     private BitbucketService bitbucketService;
 
     @Autowired
-    private GitHubService gitHubService;
+    private CommitService commitService;
+
+    @Autowired
+    private IssueService issueService;
+
+    @Autowired
+    private GitMinerService gitMinerService;
 
     @PostMapping("/{workspace}/{repo_slug}")
     public String fetchAndSendData(
@@ -30,10 +35,10 @@ public class BitbucketMinerController {
             @RequestParam(defaultValue = "5") int nIssues,
             @RequestParam(defaultValue = "2") int maxPages) {
 
-        List<Commit> commits = bitbucketService.getAllCommits(workspace, repo_slug, nCommits, maxPages);
-        List<Issue> issues = bitbucketService.getAllIssues(workspace, repo_slug, nIssues, maxPages);
+        List<Commit> commits = commitService.getAllCommits(workspace, repo_slug, nCommits, maxPages);
+        List<Issue> issues = issueService.getAllIssues(workspace, repo_slug, nIssues, maxPages);
 
-        bitbucketService.sendDataToGitMiner(commits, issues);
+        gitMinerService.sendDataToGitMiner(commits, issues);
 
         return "Datos enviados correctamente a GitMiner desde Bitbucket";
     }
@@ -47,8 +52,8 @@ public class BitbucketMinerController {
             @RequestParam(defaultValue = "2") int maxPages) {
 
         Map<String, Object> preview = new HashMap<>();
-        preview.put("commits", bitbucketService.getAllCommits(workspace, repo_slug, nCommits, maxPages));
-        preview.put("issues", bitbucketService.getAllIssues(workspace, repo_slug, nIssues, maxPages));
+        preview.put("commits", commitService.getAllCommits(workspace, repo_slug, nCommits, maxPages));
+        preview.put("issues", issueService.getAllIssues(workspace, repo_slug, nIssues, maxPages));
         return preview;
     }
 
